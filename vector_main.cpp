99c17150137c9fd89c6803c2d3c7fbe5b8d7c731
Ahmed Gamal <155440976+JEMYJYMAT@users.noreply.github.com>
2024-02-09 16:12:21 +0200

    Create vector_main.cpp

---
+++ vector_main.cpp
@@ -0,0 +1,33 @@
+/*
+ * vector main file.cpp
+ *
+ *  Created on: Feb 9, 2024
+ *      Author: Ahmed Gamal
+ */
+
+using namespace std;
+#include <iostream>
+#include "vector.h"
+
+int main()
+{
+    vector vec;
+    vec.add_element(10);
+    cout << vec;
+    vec.add_element(20);
+    vector vec2 = vec;
+    cout << vec;
+    vec2.add_element(30);
+    vec2.add_element(40);
+    cout << vec2;
+    int* arr = vec2+30;
+    for (int i = 0;i < vec2.get_size();i++)
+    {
+        if (i == vec2.get_size() - 1)
+            cout << arr[i] << endl;
+        else
+            cout << arr[i] << ",";
+    }
+
+    
+}
